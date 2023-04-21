3931009252bf2c87154cd6e11fd207043bc02801
Martin Grödl <martin@process.studio>
2023-04-21 08:01:20 +0000

    1080p30 on mobile

--- app/main.js
+++ app/main.js
@@ -63,7 +63,7 @@ import {inverseLerpClamped} from "../shared/mathUtils.js";
 
 const RES_INSTALLATION = [3840, 2160, 60];
 const RES_DESKTOP      = [1920, 1080, 60];
-const RES_MOBILE       = [1280,  720, 30];
+const RES_MOBILE       = [1920, 1080, 30];
 
 const PX_RATIO = 1;
 const SW_INSTALL = true;
